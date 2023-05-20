e54bef132119493fc214474bbde362da0ab4d68d
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-20 21:29:16 +0900

    Initial Commit

---
+++ LandMarkProject/LandMarkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkProjectApp.swift
+//  LandMarkProject
+//
+//  Created by David Goggins on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
